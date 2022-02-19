f9433a5c778bf18ddba85045e6251bc9d7b2d854
goku-g <gokarnabaskota500@gmail.com>
2022-02-20 01:22:49 +0545

    dc motor parameters

---
+++ motor_parameter.m
@@ -0,0 +1,9 @@
+
+%% parameters for inverted pendulum
+J = 0.01;     %moment of inertia of the rotor - kg.m^2
+b_m = 0.1;    %motor viscous friction constant - N.m.s
+Ke = 0.01;    %electromotive force constant - V/rad/sec
+Km = 0.01;    %motor torque constant - N.m/Amp
+R = 1;        %electric resistance - Ohm
+L = 0.5;      %electric inductance - H
+%% new section
